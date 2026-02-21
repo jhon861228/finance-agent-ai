5f76eaa79f0d2f666fa06e6f0ec8ac1accfab7e6
jhon861228 <dago861228@hotmail.com>
2026-02-21 13:23:58 -0500

    fix(iac): add catch-all proxy route to API Gateway for backend Express app

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -21,12 +21,19 @@ resource "aws_apigatewayv2_route" "telegram_route" {
   target    = "integrations/${aws_apigatewayv2_integration.telegram_lambda.id}"
 }
 
+# Catch-all route for the Express app
+resource "aws_apigatewayv2_route" "proxy" {
+  api_id    = aws_apigatewayv2_api.telegram_api.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.telegram_lambda.id}"
+}
+
 resource "aws_lambda_permission" "api_gateway" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.telegram_handler.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.telegram_api.execution_arn}/*/*/webhook"
+  source_arn    = "${aws_apigatewayv2_api.telegram_api.execution_arn}/*/*"
 }
 
 output "api_gateway_url" {
